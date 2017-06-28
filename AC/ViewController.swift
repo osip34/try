52c1d729d0e6d659500b3a7cb1bc308d10d8c526
AndreOsip <andreosip@macminissu8.training.local>
2017-06-28 12:58:40 +0300

    Initial Commit

---
+++ AC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AC
+//
+//  Created by AndreOsip on 6/28/17.
+//  Copyright © 2017 AndreOsip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
